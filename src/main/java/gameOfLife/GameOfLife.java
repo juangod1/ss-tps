620ffb7bb0d4382f10e54b236608103ad1d4e9e3
Agustina Osimani <aosimani@itba.edu.ar>
2020-03-17 12:31:31 -0300

    ADD - 40 boards

--- src/main/java/gameOfLife/GameOfLife.java
+++ src/main/java/gameOfLife/GameOfLife.java
@@ -1,23 +1,22 @@
 package gameOfLife;
 
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Random;
 
 public class GameOfLife {
     public static void main(String[] args) throws IOException {
-        generate2d();
-        generate3d();
+        //generate2d();
+        //generate3d();
     }
 
     private static void generate2d() throws IOException {
         int board = 0;
         Random random = new Random();
         ArrayList<ArrayList<ArrayList<Integer>>> states2d = new ArrayList<>();
-        double limit = 0.25;
+        double limit = 0.20;
 
-        while (board < 5) {
+        while (board < 40) {
             states2d.add(new ArrayList<>());
             for (int row=496; row<504; row++) {
                 for (int col=496; col<504; col++) {
@@ -29,15 +28,20 @@ public class GameOfLife {
                     }
                 }
             }
-            limit += 0.15;
+            if (board % 10 == 9)
+                limit += 0.15;
             board++;
         }
 
         board = 0;
+        int M;
         for (ArrayList<ArrayList<Integer>> state : states2d) {
-            gameOfLife.FilePrinter.print2d(state, 1000,1000, 2, 3, 2, 300, board);
-            gameOfLife.FilePrinter.print2d(state, 1000,1000, 1, 2, 3, 300, board+1);
-            gameOfLife.FilePrinter.print2d(state, 1000,1000, 0, 6, 5, 300, board+2);
+            M = 2;
+            gameOfLife.FilePrinter.print2d(state, 1000,1000, M, M+2, M+1, 300, board);
+            M = 3;
+            gameOfLife.FilePrinter.print2d(state, 1000,1000, M, M+2, M+1, 300, board+1);
+            M = 1;
+            gameOfLife.FilePrinter.print2d(state, 1000,1000, M, M+2, M+1, 300, board+2);
             board+=3;
         }
     }
--- src/main/java/gameOfLife/plot.py
+++ src/main/java/gameOfLife/plot.py
@@ -1,18 +1,29 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.optimize import curve_fit
 
-slopes = []
+# for i in range(15):
+    # if i==0:
+    #     continue
+data = pd.read_csv("/home/agusosimani/Documents/ss-tp1/table2d{}".format(18), names=["Generation","Cells Alive"], header=None)
+ax1 = data.plot(x="Generation", y="Cells Alive")
 
-for i in range(10):
-    if i==0:
-        continue
-    print("/home/agusosimani/Documents/ss-tp1/table3d{}".format(i))
-    data = pd.read_csv("/home/agusosimani/Documents/ss-tp1/table3d{}".format(i), names=["Generation","Cells Alive"], header=None)
-    data.plot(x="Generation", y="Cells Alive")
+def exponential(x, a, b):
+    return a*np.exp(b*x)
 
-    log_y_data = np.log(data["Cells Alive"])
-    curve_fit = np.polyfit(data["Generation"], log_y_data, 1)
-    print(curve_fit)
+param, param_cov = curve_fit(exponential, np.array(data["Generation"]), np.array(data["Cells Alive"]),[71.65,0.07835])
+print(param)
+
+data = pd.read_csv("/home/agusosimani/Documents/ss-tp1/table2d{}".format(19), names=["Generation","Cells Alive"], header=None)
+ax2 = data.plot(x="Generation", y="Cells Alive")
+data = pd.read_csv("/home/agusosimani/Documents/ss-tp1/table2d{}".format(20), names=["Generation","Cells Alive"], header=None)
+ax3 = data.plot(x="Generation", y="Cells Alive")
+
+
+
+
+# log_y_data = np.log(data["Cells Alive"])
+    # curve_fit = np.polyfit(data["Generation"], log_y_data, 1)
+    # print(curve_fit)
 plt.show()
-print(slopes)
