c530aa8e6730c2f8da7d7a4220865156c1b080f4
Agustina Osimani <aosimani@itba.edu.ar>
2020-04-01 15:49:03 -0300

    ADD - P vs T plot

--- src/main/java/gasDiffusion/plot.py
+++ src/main/java/gasDiffusion/plot.py
@@ -5,47 +5,74 @@ from scipy.optimize import curve_fit
 from scipy.stats import linregress
 import statistics
 
-### PLOT FP VS T - N VARIATION ###
+### PLOT FP VS T - A VARIATION ###
 
-data = pd.read_csv("/home/agusosimani/Documents/ss-tp1/src/main/java/gasDiffusion/output/cien/table{}".format(0), names=["Time", "FP", "Pressure","Temp"], header=None)
-plt.plot(data['Time'], data['FP'], label="N=100")
-data = pd.read_csv("/home/agusosimani/Documents/ss-tp1/src/main/java/gasDiffusion/output/cincuenta/table{}".format(0), names=["Time", "FP", "Pressure","Temp"], header=None)
-plt.plot(data['Time'], data['FP'], label="N=50")
-data = pd.read_csv("/home/agusosimani/Documents/ss-tp1/src/main/java/gasDiffusion/output/veinticinco/table{}".format(0), names=["Time", "FP", "Pressure","Temp"], header=None)
-plt.plot(data['Time'], data['FP'], label="N=25")
-plt.xlabel("Time [s]")
-plt.ylabel("fp")
-plt.legend(loc='best')
-plt.show()
+# data = pd.read_csv("/home/agusosimani/Documents/ss-tp1/src/main/java/gasDiffusion/output/cerouno/table{}".format(12), names=["Time", "FP", "Pressure","Temp"], header=None)
+# plt.plot(data['Time'], data['FP'], label="A=0.01")
+# data = pd.read_csv("/home/agusosimani/Documents/ss-tp1/src/main/java/gasDiffusion/output/cerotres/table{}".format(0), names=["Time", "FP", "Pressure","Temp"], header=None)
+# plt.plot(data['Time'], data['FP'], label="A=0.03")
+# data = pd.read_csv("/home/agusosimani/Documents/ss-tp1/src/main/java/gasDiffusion/output/cerocinco/table{}".format(6), names=["Time", "FP", "Pressure","Temp"], header=None)
+# plt.plot(data['Time'], data['FP'], label="A=0.05")
+# plt.xlabel("Tiempo [s]")
+# plt.ylabel("Fracción de partículas en recinto izquierdo")
+# plt.legend(loc='best')
+# plt.grid()
+# plt.show()
 
 ### PLOT INPUT VS OUTPUT T of FP ~ 0.5 VS N ###
 
-values = {25: [], 50: [], 100: []}
+# values = {0.01: [], 0.02: [], 0.03: [], 0.04: [], 0.05: [], 0.06: []}
+#
+# def exponential(x, a, b):
+#     return a*np.exp(b*x)
+#
+# for i in range(20):
+#     data01 = pd.read_csv("/home/agusosimani/Documents/ss-tp1/src/main/java/gasDiffusion/output/cerouno/table{}".format(i), names=["Time", "FP", "Pressure","Temp"], header=None)
+#     values[0.01].append(data01['Time'].max())
+#     data02 = pd.read_csv("/home/agusosimani/Documents/ss-tp1/src/main/java/gasDiffusion/output/cerodos/table{}".format(i), names=["Time", "FP", "Pressure","Temp"], header=None)
+#     values[0.02].append(data02['Time'].max())
+#     data03 = pd.read_csv("/home/agusosimani/Documents/ss-tp1/src/main/java/gasDiffusion/output/cerotres/table{}".format(i), names=["Time", "FP", "Pressure","Temp"], header=None)
+#     values[0.03].append(data03['Time'].max())
+#     data04 = pd.read_csv("/home/agusosimani/Documents/ss-tp1/src/main/java/gasDiffusion/output/cerocuatro/table{}".format(i), names=["Time", "FP", "Pressure","Temp"], header=None)
+#     values[0.04].append(data04['Time'].max())
+#     data05 = pd.read_csv("/home/agusosimani/Documents/ss-tp1/src/main/java/gasDiffusion/output/cerocinco/table{}".format(i), names=["Time", "FP", "Pressure","Temp"], header=None)
+#     values[0.05].append(data05['Time'].max())
+#     data06 = pd.read_csv("/home/agusosimani/Documents/ss-tp1/src/main/java/gasDiffusion/output/ceroseis/table{}".format(i), names=["Time", "FP", "Pressure","Temp"], header=None)
+#     values[0.06].append(data06['Time'].max())
+#
+# a = [0.01,0.02,0.03,0.04,0.05,0.06]
+# b = [statistics.mean(values[0.01]), statistics.mean(values[0.02]), statistics.mean(values[0.03]), statistics.mean(values[0.04]), statistics.mean(values[0.05]), statistics.mean(values[0.06])]
+# c = [statistics.stdev(values[0.01]), statistics.stdev(values[0.02]), statistics.stdev(values[0.03]), statistics.stdev(values[0.04]), statistics.stdev(values[0.05]), statistics.stdev(values[0.06])]
+# plt.errorbar(a,b,yerr=c, linestyle="None", mfc='red', fmt='-o')
+# plt.xlabel('Tamaño de la apertura [m]')
+# plt.ylabel('Tiempo estacionario [s]')
+# plt.xticks([0.01,0.02,0.03,0.04,0.05,0.06])
+# plt.grid()
+# plt.show()
 
-def exponential(x, a, b):
-    return a*np.exp(b*x)
+### PLOT P VS T ###
+
+values = {0.01: [], 0.05: [], 0.1: [], 0.15: [], 0.2: [], 0.25: []}
 
 for i in range(20):
-    data25 = pd.read_csv("/home/agusosimani/Documents/ss-tp1/src/main/java/gasDiffusion/output/veinticinco/table{}".format(i), names=["Time", "FP", "Pressure","Temp"], header=None)
-    values[25].append(data25['Time'].max())
-    data50 = pd.read_csv("/home/agusosimani/Documents/ss-tp1/src/main/java/gasDiffusion/output/cincuenta/table{}".format(i), names=["Time", "FP", "Pressure","Temp"], header=None)
-    values[50].append(data50['Time'].max())
-    data100 = pd.read_csv("/home/agusosimani/Documents/ss-tp1/src/main/java/gasDiffusion/output/cien/table{}".format(i), names=["Time", "FP", "Pressure","Temp"], header=None)
-    values[100].append(data100['Time'].max())
+    data01 = pd.read_csv("/home/agusosimani/Documents/ss-tp1/src/main/java/gasDiffusion/output/velocities/cerouno/table{}".format(i), names=["Temp", "Pressure"], header=None)
+    values[0.01].append(data01['Pressure'].max())
+    data05 = pd.read_csv("/home/agusosimani/Documents/ss-tp1/src/main/java/gasDiffusion/output/velocities/cerocinco/table{}".format(i), names=["Temp", "Pressure"], header=None)
+    values[0.05].append(data05['Pressure'].max())
+    data1 = pd.read_csv("/home/agusosimani/Documents/ss-tp1/src/main/java/gasDiffusion/output/velocities/uno/table{}".format(i), names=["Temp", "Pressure"], header=None)
+    values[0.1].append(data1['Pressure'].max())
+    data15 = pd.read_csv("/home/agusosimani/Documents/ss-tp1/src/main/java/gasDiffusion/output/velocities/unocinco/table{}".format(i), names=["Temp", "Pressure"], header=None)
+    values[0.15].append(data15['Pressure'].max())
+    data2 = pd.read_csv("/home/agusosimani/Documents/ss-tp1/src/main/java/gasDiffusion/output/velocities/dos/table{}".format(i), names=["Temp", "Pressure"], header=None)
+    values[0.2].append(data2['Pressure'].max())
+    data25 = pd.read_csv("/home/agusosimani/Documents/ss-tp1/src/main/java/gasDiffusion/output/velocities/doscinco/table{}".format(i), names=["Temp", "Pressure"], header=None)
+    values[0.25].append(data25['Pressure'].max())
 
-a = [25,50,100]
-b = [statistics.mean(values[25]), statistics.mean(values[50]), statistics.mean(values[100])]
-c = [statistics.stdev(values[25]), statistics.stdev(values[50]), statistics.stdev(values[100])]
-plt.errorbar(a,b,yerr=c, linestyle="None", mfc='red', fmt='-o')
-plt.xlabel('N')
-plt.ylabel('T estacionario [s]')
-plt.xticks([25,50,100])
+a = [1.2071626270176518E20,3.0179065675441316E21,1.2071626270176526E22,2.716115910789715E22,4.8286505080706105E22,7.544766418860323E22]
+b = [statistics.mean(values[0.01]), statistics.mean(values[0.05]), statistics.mean(values[0.1]), statistics.mean(values[0.15]), statistics.mean(values[0.2]), statistics.mean(values[0.25])]
+c = [statistics.stdev(values[0.01]), statistics.stdev(values[0.05]), statistics.stdev(values[0.1]), statistics.stdev(values[0.15]), statistics.stdev(values[0.2]), statistics.stdev(values[0.25])]
+plt.errorbar(a,b,yerr=c, mfc='red', fmt='-o')
+plt.xlabel('Temperatura [K]')
+plt.ylabel('Presión [N/m^2]')
+plt.grid()
 plt.show()
-
-
-# ax = data.plot(x="Time", y="Pressure")
-# ax.set_xlim(data["Time"].min(), data["Time"].max())
-# ax = data.plot(x="Time", y="Temp")
-# plt.plot(data['Time'], data['Temp'])
-# plt.xlim((data["Time"].min(), data["Time"].max()))
-# # plt.ylim((0.0000001*data["Temp"].min(), data["Temp"].max()))
