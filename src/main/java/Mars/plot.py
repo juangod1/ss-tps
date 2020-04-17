dfdd76ce6b4272fd99d4b803f42a04dec0c2211c
Agustina Osimani <aosimani@itba.edu.ar>
2020-04-17 18:38:16 -0300

    ADD - more plots

--- src/main/java/Mars/plot.py
+++ src/main/java/Mars/plot.py
@@ -40,3 +40,23 @@ plt.xlabel("Minutos transcurridos a partir de las 00:00hs del 14/7/2020")
 plt.ylabel("Distancia de la nave a Marte [m]")
 plt.grid()
 plt.show()
+
+### PLOT DISTANCE VS DAY v0=8 DOUBLE LAUNCH DISTANCE ###
+
+data = pd.read_csv("/home/agusosimani/Documents/ss-tp1/src/main/java/Mars/graficos/closestDistanceV8withDOUBLEorbitalDISTANCE", names=["day", "distance",], header=None)
+plt.plot(data['day'], data['distance'], "o", linestyle="None", color="blue", markersize=2)
+plt.yscale('log')
+plt.xlabel("Días transcurridos desde 6/4/2020")
+plt.ylabel("Distancia de la nave a Marte [m]")
+plt.grid()
+plt.show()
+
+### PLOT DISTANCE VS DAY ZOOM v0=8 DOUBLE LAUNCH DISTANCE ###
+
+data = pd.read_csv("/home/agusosimani/Documents/ss-tp1/src/main/java/Mars/graficos/closestDistanceV8withDOUBLEorbitalDISTANCEevery8Minutes", names=["day", "distance",], header=None)
+plt.plot(data['day'], data['distance'], "o", linestyle="None", color="blue", markersize=2)
+plt.yscale('log')
+plt.xlabel("Minutos transcurridos a partir de las 00:00hs del 15/7/2020")
+plt.ylabel("Distancia de la nave a Marte [m]")
+plt.grid()
+plt.show()
